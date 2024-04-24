70b56333e5624d177288d1f378be20237c0f9644
Leonidus786 <abhi.upadh@gmail.com>
2024-04-24 22:39:26 +0530

    revision

---
+++ Projects/P_2- Tip Calculator/start.py
@@ -0,0 +1,24 @@
+#If the bill was $150.00, split between 5 people, with 12% tip. 
+
+#Each person should pay (150.00 / 5) * 1.12 = 33.6
+#Format the result to 2 decimal places = 33.60
+
+#Tip: There are 2 ways to round a number. You might have to do some Googling to solve this.💪
+
+#Write your code below this line 👇
+
+# welcome to the final project of today. And in this final project, we're going to be building a Tip Calculator.
+# It's going to print "Welcome to the tip calculator!"
+# and it's going to ask you for an input for how much the total bill came to.
+# Let's say that we and a couple of friends went out for lunch and it came to $124.56.
+# And then it'll ask you, what percentage tip would you like to give? 
+# So let's say we want to give a 12% tip. So we enter 12 and hit Enter.
+# And then finally it asks, how many people are splitting this bill?
+# Let's say there's seven of us.And then hit Enter.
+# Finally, it's going to tell us that each person should pay about $19.93.
+# Notice how the final bill, even though after all of these calculations,
+# it's probably got more numbers after the decimal point, we only want two decimal places of accuracy,
+# so it should be rounded to two decimal places.
+# The second thing to remember is that these are percentages, and in order to calculate a percentage of something,
+# you can multiply a number by the percentage number divided by 100.
+# Let me show you this in a little bit more detail,
