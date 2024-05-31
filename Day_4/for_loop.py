7145ddbc79ac20d41cd53ce212c48913afea9a10
Leonidus786 <abhi.upadh@gmail.com>
2024-05-31 10:40:02 +0530

    for loop - and assignment on pattern.

---
+++ Day_4/for_loop.py
@@ -0,0 +1,11 @@
+# my_name = "Amit"
+
+# for i in my_name:
+#     print(i)
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+
+# https://www.geeksforgeeks.org/programs-printing-pyramid-patterns-python/
---
+++ Day_4/loops.py
@@ -0,0 +1,24 @@
+# i = 1
+# while i < 6:
+#   print(i)
+#   i += 1
+
+# j = 1
+# while j < 21:
+#     print(j)
+#     j += 1
+
+# i = 1
+# while i < 6:
+#   print(i)
+#   if i == 5:
+#     break
+#   i += 1
+
+i = 0
+while i < 6:
+  i += 1
+  if i == 3:
+    continue
+  print(i)
+  
